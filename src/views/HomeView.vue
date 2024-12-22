649cd138102f044e444917969906b9f0d0dafc89
MarwenSAIDI <marouene.saidi.ds@gmail.com>
2024-12-22 11:50:14 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ratione corrupti libero nemo facilis voluptates animi possimus perspiciatis cumque provident unde culpa accusamus distinctio aspernatur consectetur, ducimus similique vitae maxime alias minima voluptatibus tempora. Officiis doloremque autem rem saepe recusandae doloribus odit sunt expedita, perferendis officia dolores cum sed ab. At sit, sint nihil harum consequuntur rem nam possimus ipsa veritatis eius odio voluptas sapiente inventore culpa laudantium, numquam porro nisi, maxime libero molestiae. Reprehenderit expedita dolorum id. Amet, provident dicta omnis vitae maiores veniam unde tempore labore itaque ipsum, quis commodi perspiciatis? Ab, esse cum beatae minima dolorem dignissimos?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ratione corrupti libero nemo facilis voluptates animi possimus perspiciatis cumque provident unde culpa accusamus distinctio aspernatur consectetur, ducimus similique vitae maxime alias minima voluptatibus tempora. Officiis doloremque autem rem saepe recusandae doloribus odit sunt expedita, perferendis officia dolores cum sed ab. At sit, sint nihil harum consequuntur rem nam possimus ipsa veritatis eius odio voluptas sapiente inventore culpa laudantium, numquam porro nisi, maxime libero molestiae. Reprehenderit expedita dolorum id. Amet, provident dicta omnis vitae maiores veniam unde tempore labore itaque ipsum, quis commodi perspiciatis? Ab, esse cum beatae minima dolorem dignissimos?</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
